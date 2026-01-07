d530e2fd4bf7cbaf8a7683dfd27870f033a3abf4
octaviobottari <octaviobottari7@gmail.com>
2026-01-07 17:09:22 -0300

    Fix cart functionality - add multiple products to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart") || [];
+
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
+
+  console.log("Producto agregado al carrito:", product.Name);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
